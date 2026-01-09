0cb813d11b911d74a8bf6ee485fee380f027575b
Jolaenes <jjhorbanowicz@gmai.com>
2026-01-09 19:21:40 +0100

    DZIEN2

--- models.py
+++ models.py
@@ -29,9 +29,21 @@ def init_db():
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         chat_id INTEGER NOT NULL,
         user TEXT NOT NULL,
-        content TEXT NOT NULL
+        content TEXT NOT NULL,
+        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
     )
     """)
+    cursor.execute("""
+CREATE TABLE IF NOT EXISTS chat_users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    chat_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY(chat_id) REFERENCES chats(id),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    UNIQUE(chat_id, user_id)
+)
+""")
+
 
     db.commit()
     db.close()
