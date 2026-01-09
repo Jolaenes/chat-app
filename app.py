8f81371b509244715bff9654ef673d0f7022e102
Jolaenes <jjhorbanowicz@gmai.com>
2026-01-09 17:37:45 +0100

    DZIEN4

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, request, jsonify, render_template
-from werkzeug.security import generate_password_hash
+from flask import Flask, request, jsonify, render_template, session
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import redirect
 import sqlite3
 from models import init_db  # importujemy init_db()
 
 app = Flask(__name__)
+
+# Sesja użytkownika
+app.secret_key = "dev-secret-key"
 
 # Inicjalizacja bazy przy starcie
 init_db()
@@ -45,6 +49,64 @@ def register_user():
         return jsonify({"message": "User registered successfully"}), 201
     except sqlite3.IntegrityError:
         return jsonify({"error": "Username already exists"}), 400
+
+
+
+# LOGOWANIE UŻYTKOWNIKA
+
+# Formularz logowania (GET)
+@app.route("/login", methods=["GET"])
+def login_form():
+    return render_template("login.html")
+
+# Logowanie (POST)
+@app.route("/login", methods=["POST"])
+def login_user():
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT password FROM users WHERE username = ?",
+        (username,)
+    )
+    user = cursor.fetchone()
+    conn.close()
+
+    if not user or not check_password_hash(user[0], password):
+        return jsonify({"error": "Invalid username or password"}), 401
+
+    # Zapis sesji
+    session["user"] = username
+
+    return jsonify({"redirect": "/chat"}), 200
+
+
+
+
+# SESJA / WYLOGOWANIE
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return jsonify({"message": "Logged out"}), 200
+
+
+@app.route("/me")
+def me():
+    if "user" not in session:
+        return jsonify({"error": "Not logged in"}), 401
+    return jsonify({"user": session["user"]})
+
+# widok chat
+@app.route("/chat")
+def chat():
+    if "user" not in session:
+        return redirect("/login")
+    return render_template("chat.html", user=session["user"])
+
 
 # Uruchomienie serwera
 if __name__ == "__main__":
